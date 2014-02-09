186acba87af096366b1b7fc7dc6d6c79d3719c9a
Anton S <ijon57@gmail.com>
2014-02-09 19:24:49 +0100

    added spec for recent resures scope

--- spec/models/treasure_spec.rb
+++ spec/models/treasure_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Treasure do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe ".recent" do
+    it "returns X recent treasures (5 by default)" do
+      FactoryGirl.create_list(:treasure, 3)
+      Treasure.recent(2).should have(2).entries
+    end
+  end
+
 end
