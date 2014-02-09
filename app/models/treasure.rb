7096e943c545a45ebf3f5434aae547c9d806767d
Anton S <ijon57@gmail.com>
2014-02-09 19:29:07 +0100

    link to treasure

--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,6 +1,6 @@
 class Treasure < ActiveRecord::Base
 
-  scope :recent, ->(num){ order('created_at desc').limit(num) }
+  scope :recent, ->(num=5){ order('created_at desc').limit(num) }
 
   attr_accessible :name
   validates :name, presence: true, uniqueness: {scope: :user_id}
