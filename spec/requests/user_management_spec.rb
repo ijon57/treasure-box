b740a72d071201e64702356ac0579640d3a51416
Anton S <ijon57@gmail.com>
2014-02-09 17:40:54 +0100

    added request spec for user managmenet

--- spec/requests/user_management_spec.rb
+++ spec/requests/user_management_spec.rb
@@ -3,10 +3,18 @@ require "spec_helper"
 describe "User management" do
 
   it "creates user and redirects to user's page" do
+    get "/users/new"
+    expect(response).to render_template(:new)
+
     email = "user@mail.test"
     password = "secretpassword"
-    post "/users", email: email, password: password, password_confirmation: password
+    post "/users", user: {email: email, password: password}
     expect(response).to redirect_to(assigns(:user))
+    
+    follow_redirect!
+
+    expect(response).to render_template(:show)
+    expect(response.body).to include("User successfully created")
   end
 
 end
