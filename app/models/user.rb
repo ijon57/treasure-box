092a2aa92c494bfe54c433daa307fedfbc7e083e
Anton S <ijon57@gmail.com>
2014-02-09 19:23:59 +0100

    specs for user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: /\A.+@.+\..+\Z/
   validates :password, presence: true, on: :create
 
   has_many :treasures
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "creating new user" do
+
+    it "is invalid without email" do
+      user = User.new
+      user.should be_invalid
+    end
+
+    it "is invalid with invalid email" do
+      user = User.new(email: "test", password: "testpassword")
+      user.should be_invalid
+    end
+
+    it "is invalid if password is missing" do
+      user = User.new(email: "user@mail.test")
+      user.should be_invalid
+    end
+
+    it "is valid if email and password are valid" do
+      user = User.new(email: "user@mail.test", password: "testpassword")
+      user.should be_valid
+    end
+  end
 end
