914dc1386508eaef40d8f0cb84aa7f7d022a7d6c
Anton S <ijon57@gmail.com>
2014-02-09 19:23:44 +0100

    added treasure factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@mail.test" }
+    password "123TestPassword"
+  end
+
+  factory :treasure do
+    user
+    sequence(:name) {|n| "treasure #{n}" }
   end
 end
