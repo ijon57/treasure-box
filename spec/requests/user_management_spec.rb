675f9f4ab68fc2235b0dc39b8ce54e9467afc05f
Anton S <ijon57@gmail.com>
2014-02-09 18:07:04 +0100

    added additional test for invalid user data

--- spec/requests/user_management_spec.rb
+++ spec/requests/user_management_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "User management" do
 
-  it "creates user and redirects to user's page" do
+  it "creates user and redirects to homepage" do
     get "/users/new"
     expect(response).to render_template(:new)
 
@@ -15,6 +15,15 @@ describe "User management" do
 
     expect(response).to render_template(:index)
     expect(response.body).to include("User successfully created")
+    expect(response.body).to include("Logged in as: #{email}")
+  end
+
+  it "doesn't create user and shows form again" do
+    get "/users/new"
+    expect(response).to render_template(:new)
+
+    post "/users"
+    expect(response).to render_template(:new)
   end
 
 end
